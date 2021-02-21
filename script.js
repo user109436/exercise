0b53d98bfdf0a77c6e256a2b10d6c89efc8dec80
kedez <tsukoyomi243@gmail.com>
2021-02-20 21:15:53 -0500

    Javascript Playground

--- script.js
+++ script.js
@@ -1,1 +1,137 @@
-console.log('Javscript link loaded');
+// var inputs = document.getElementsByClassName('fields');
+// console.log(inputs);
+
+    
+    // for (var i = 0; i < inputs.length; i++) {
+    //     inputs[i].addEventListener('click', function () {
+    //         // console.log(this.value = "Bobby");
+    //     })
+
+    //     // inputs[i].classList.add('add');
+    // }
+// $(document).ready(function () {
+
+//     console.log('Javscript link loaded');
+
+// });
+
+
+// var pa = document.getElementById('paragraph');
+// console.log(pa);
+
+
+// pa.addEventListener('click', function(){
+// pa.classList.add('hide');
+// });
+
+
+
+// function add(x=0, y=0) { 
+//     return x + y;
+// }
+// console.log(add());
+// console.log(add(2, 2));
+
+// const menu = document.getElementById('menu');
+// const bg = document.getElementById('background');
+// var menuOpen = false;
+
+/*
+false -open  !menuOPen=true
+
+
+*/
+// menu.addEventListener('click', function () {
+//     if (!menuOpen) {
+//         menuOpen = true;
+//         console.log(menuOpen);
+//         bg.classList.add('expand');
+//     } else { 
+//         menuOpen = false;
+//         console.log(menuOpen);
+//         bg.classList.remove('expand');
+//     }
+// });
+
+
+
+
+// var fname = document.getElementById('name');
+// console.log(fname.value);
+
+// var fields = document.getElementsByClassName('fields');
+// console.log(fields);
+
+// setTimeout(function () {
+//     console.log("Hello world");
+// }, 1000);
+
+// var i = 0;
+// let j = 0;
+// const INCREMENT= 0;
+// setInterval(function () {
+//     i++;
+//     console.log("Hello world "+ i);
+
+// }, 1000);
+
+// document.write('HTML replaced');
+// alert("YOu have new notification");
+// let x = confirm("Do you want to delete this data ID:1");
+
+
+// if (x) {
+//     console.log(x);
+//     console.log("Yes");
+// } else { 
+//     console.log(x);
+//     console.log("NO");
+// }
+
+// let age = prompt("Your age?", "18"); 
+
+// if (age != null) {
+//     console.log(age);
+// } else { 
+//     console.log("Age is null");
+// }
+
+
+// let id = '1wasdfas';
+// let newId = parseInt(id);
+// if (newId && id > 0) {
+//     console.log(`${newId} parsed`);
+// } else { 
+//     console.log("not parsed: " + id);
+// }
+// console.log(1+1);
+
+
+// var dogs = [ "Bulldog", "Beagle", "Labrador"]; 
+// dogs.push("Chihuahua");
+// console.log(dogs);
+// dogs.pop();
+// console.log(dogs);
+
+// var fname = "Bobby";
+
+// console.log(fname.length);
+
+// for (var i = 0; i < fname.length; i++) {     // parsing with array.length
+//       console.log(`index:${i} value:${fname[i]}`);
+// }
+
+// fname.forEach(function (value, i) { 
+//     console.log(value);
+// });
+// console.log(`Length:${dogs.length}`);
+
+// console.log(dogs[1]);
+
+// for (var i = 0; i < dogs.length; i++) {     // parsing with array.length
+//       console.log(`index:${i} value:${dogs[i]}`);
+// }
+// dogs.forEach(function (value, i) {
+//     console.log(`value: ${value} index:${i}`);
+// });
+
